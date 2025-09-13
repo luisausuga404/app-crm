e00397a7fb8d82b93c3adcdd358cffe45dde5a04
luisausuga404 <lusuga404@gmail.com>
2025-09-13 14:53:19 -0500

    (develop) inicio de validacion con expresiones regulares

---
+++ helpers/validarRegistro.js
@@ -0,0 +1,11 @@
+let inputs = document.getElementsByClassName("formulario_entrada")
+let arregloInputs = [...inputs]
+    console.log(inputs)
+    console.log(arregloInputs)
+
+    arregloInputs.map((input)=> {
+        console.log(input)
+    })
+
+
+
