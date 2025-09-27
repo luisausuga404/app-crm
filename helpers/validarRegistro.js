f118f87b15b1ade4350ee1f2111cf23fd2dc31f7
lusuga404 <lusuga404@gmail.com>
2025-09-27 14:45:35 -0500

    login de la plataforma skillphere donde esta relacionado el encarpetado

--- helpers/validarRegistro.js
+++ helpers/validarRegistro.js
@@ -1,11 +1,32 @@
-let inputs = document.getElementsByClassName("formulario_entrada")
+import { expresiones } from "./expresionesexpresiones.js"
+let inputs = document.getElementsByClassName("formulario__entrada")
 let arregloInputs = [...inputs]
-    console.log(inputs)
-    console.log(arregloInputs)
-
-    arregloInputs.map((input)=> {
-        console.log(input)
-    })
-
-
-
+arregloInputs.map((input) => {
+    input.addEventListener("keyup", (e) => {
+        switch (e.target.id) {
+            case "nombre":
+                if (expresiones.texto.test(e.target.value)) {
+                    e.target.style.borderColor = "green"
+                } else {
+                    document.getElementById("nombre").style.borderColor = "red"
+                }
+                break;
+            case "apellido":
+                if (expresiones.texto.test(e.target.value)) {
+                    e.target.classList.add("correcto")
+                    e.target.classList.remove("incorrecto")
+                } else {
+                    e.target.classList.add("incorrecto")
+                    e.target.classList.remove("correcto")
+                }
+                break
+            case "documento":
+                if (expresiones.numero.test(e.target.value)) {
+                    e.target.classList.add("correcto")
+                    e.target.classList.remove("incorrecto")
+                } else {
+                    e.target.classList.add("incorrecto")
+                    e.target.classList.remove("correcto")
+                }
+                break
+        }
